5ae7e3ae547daf807080e8c84c727a4c27e91c78
Gustavo <gustavomendes.medeiros@gmail.com>
2020-02-13 00:29:59 -0300

    Create FlashInfo Schema and index, show and store methods

--- src/app/controllers/FlashInfoController.js
+++ src/app/controllers/FlashInfoController.js
@@ -1,5 +1,32 @@
+import FlashInfo from '../schemas/FlashInfo';
+
 class FlashInfoController {
-  async show() {}
+  async index(req, res) {
+    const flashInfos = await FlashInfo.find({});
+
+    return res.json(flashInfos);
+  }
+
+  async show(req, res) {
+    const { id } = req.params;
+
+    const flashInfo = await FlashInfo.findById(id);
+
+    return res.json(flashInfo);
+  }
+
+  async store(req, res) {
+    const { title, body, image, schedule } = req.body;
+
+    const flashInfo = await FlashInfo.create({
+      title,
+      body,
+      image,
+      schedule,
+    });
+
+    return res.json(flashInfo);
+  }
 }
 
 export default new FlashInfoController();
---
+++ src/app/schemas/FlashInfo.js
@@ -0,0 +1,31 @@
+import { Schema, model } from 'mongoose';
+
+const FlashInfoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    schedule: {
+      type: Number,
+      required: true,
+    },
+    published: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default model('FlashInfo', FlashInfoSchema);
